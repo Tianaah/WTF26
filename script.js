53e1930c2d982293cb048e498af01a5aba8872bf
Tianaah <christiana15ako@gmail.com>
2025-09-04 15:13:20 +0100

    new updates

---
+++ script.js
@@ -0,0 +1,15 @@
+let num1 = 12;
+let num2 = 4;
+const name = "Felicia";
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+
+console.log("Hello " + name + ", the result of num1 + num2 is " + addition);
