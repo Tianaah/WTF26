7b4799be1c1363f4380d5fb1d37c0fed0bc12494
Tianaah <christiana15ako@gmail.com>
2025-09-10 08:37:50 +0100

    new commits

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
-let num1 = 12;
-let num2 = 4;
-const name = "Felicia";
+let highestBalance;
+if (
+  accountOne.balance > accountTwo.balance &&
+  accountOne.balance > accountThree.balance
+) {
+  highestBalance = accountOne.balance;
+  console.log(`Highest Balance is ${highestBalance}`);
+} else if (
+  accountTwo.balance > accountOne.balance &&
+  accountTwo.balance > accountThree.balance
+) {
+  highestBalance = accountTwo.balance;
+  console.log(`Highest Balance is ${highestBalance}`);
+} else if (
+  accountThree.balance > accountOne.balance &&
+  accountThree.balance > accountTwo.balance
+) {
+  highestBalance = accountThree.balance;
+  console.log(`Highest Balance is ${highestBalance}`);
+} else if (
+  accountOne.balance == accountTwo.balance ||
+  accountTwo.balance == accountThree.balance ||
+  accountOne.balance == accountThree.balance
+) {
+  console.log("Two accounts are equal");
+}
 
-let addition = num1 + num2;
-let subtraction = num1 - num2;
-let multiplication = num1 * num2;
-let division = num1 / num2;
+let Status;
+if (accountOne.balance > 0) {
+  Status = "Active";
+  console.log(`Account One is ${Status}`);
+} else if (accountOne.balance === 0) {
+  Status = "Inactive";
+  console.log(`Account One is ${Status}`);
+} else {
+  Status = "Dormant";
+  console.log(`Account One is ${Status}`);
+}
 
-console.log(addition);
-console.log(subtraction);
-console.log(multiplication);
-console.log(division);
+if (accountTwo.balance > 0) {
+  Status = "Active";
+  console.log(`Account Two is ${Status}`);
+} else if (accountTwo.balance === 0) {
+  Status = "Inactive";
+  console.log(`Account Two is ${Status}`);
+} else {
+  Status = "Dormant";
+  console.log(`Account Two is ${Status}`);
+}
 
-console.log("Hello " + name + ", the result of num1 + num2 is " + addition);
+if (accountThree.balance > 0) {
+  Status = "Active";
+  console.log(`Account Three is ${Status}`);
+} else if (accountThree.balance === 0) {
+  Status = "Inactive";
+  console.log(`Account Three is ${Status}`);
+} else {
+  Status = "Dormant";
+  console.log(`Account Three is ${Status}`);
+}
